6b0de5b0b469e3f8f4191bdcf36ea9ce482c3596
Demetris Stavrou <1180929+demestav@users.noreply.github.com>
2019-03-23 11:50:59 +0200

    Absolute imports.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,8 +1,8 @@
 from fabric import task
 from invoke import Collection
-import local
-import production
+import fabfile.local
+import fabfile.production
 
 ns = Collection()
-ns.add_collection(Collection.from_module(local))
-ns.add_collection(Collection.from_module(production))
+ns.add_collection(Collection.from_module(fabfile.local))
+ns.add_collection(Collection.from_module(fabfile.production))
